1269e187c5b241b4b1a126955fa9bae5bcdd65b1
ShuoZheLi <37442704+ShuoZheLi@users.noreply.github.com>
2019-06-09 20:19:16 -0500

    1.Basic Syntax
    Basic Syntax

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main (String[] args) {
+		System.out.println("Hello");
+		System.out.print("This is Mike");
+		System.out.print(". He is a doctor.");
+		System.out.println();
+		System.out.print("What is your plan?");
+		System.out.println("\tGo home");
+		System.out.print("Okay");
+	}
+}
